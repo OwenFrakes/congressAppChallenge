902ec84954854cbedacfb1e566cc1170b5623553
JoshuaWorkman08 <joshua.workman@stu.daviess.kyschools.us>
2024-09-04 13:06:06 -0500

    Spin Button (Node Not Found Error)
    Working on fixing the error, Something to do with the node pathing.

--- Scripts/slots.gd
+++ Scripts/slots.gd
@@ -3,5 +3,8 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	print_tree_pretty()
+	
+func _on_spin_button_pressed() -> void:
+	$SpinnyPartL.position.y = 0
 	
