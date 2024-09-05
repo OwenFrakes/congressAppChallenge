d7ae7cd87b581fd33c50a1dfbb2535ab57232796
JoshuaWorkman08 <joshua.workman@stu.daviess.kyschools.us>
2024-09-05 14:42:29 -0500

    Spinny Part Fixed
    Button got disconnected from the script somehow but it works now.

--- Scripts/slots.gd
+++ Scripts/slots.gd
@@ -6,6 +6,11 @@ func _ready() -> void:
 	print_tree_pretty()
 	
 func _on_spin_button_pressed() -> void:
+	print("button preseed")
 	$"../ClippingMaskL/SpinnyPartL".position.y = randi_range(173, 682)
 	$"../ClippingMaskM/SpinnyPartM".position.y = randi_range(173, 682)
 	$"../ClippingMaskR/SpinnyPartR".position.y = randi_range(173, 682)
+
+
+func _on_pressed() -> void:
+	_on_spin_button_pressed() # Replace with function body.
