df0b1dee5c09bd044c1227105ae339b526a68251
JoshuaWorkman08 <joshua.workman@stu.daviess.kyschools.us>
2024-09-06 14:47:58 -0500

    Cleaned Up Code
    Added Variables for the positions of the spinny parts

--- Scripts/slots.gd
+++ Scripts/slots.gd
@@ -12,22 +12,25 @@ func _on_spin_button_pressed() -> void:
 	$"../ClippingMaskM/SpinnyPartM".position.y = randi_range(-173,189)
 	$"../ClippingMaskR/SpinnyPartR".position.y = randi_range(-173,189)
 	
+	var SpinnyPartL = $"../ClippingMaskL/SpinnyPartL".position.y
+	var SpinnyPartM = $"../ClippingMaskM/SpinnyPartM".position.y
+	var SpinnyPartR = $"../ClippingMaskR/SpinnyPartR".position.y
 	#Place the Other Half (Left)
-	if $"../ClippingMaskL/SpinnyPartL".position.y > 16: 
-		$"../ClippingMaskL/SpinnyPartTL".position.y = $"../ClippingMaskL/SpinnyPartL".position.y - 95
-	elif $"../ClippingMaskL/SpinnyPartL".position.y < 16: #working
-		$"../ClippingMaskL/SpinnyPartTL".position.y = $"../ClippingMaskL/SpinnyPartL".position.y + 300
+	if SpinnyPartL > 16: 
+		$"../ClippingMaskL/SpinnyPartTL".position.y = SpinnyPartL - 95
+	elif SpinnyPartL < 16: #working
+		$"../ClippingMaskL/SpinnyPartTL".position.y = SpinnyPartL + 300
 		
 	#Place the Other Half (Middle)
-	if $"../ClippingMaskM/SpinnyPartM".position.y > 16:
-		$"../ClippingMaskM/SpinnyPartTM".position.y = $"../ClippingMaskM/SpinnyPartM".position.y - 95
-	elif $"../ClippingMaskM/SpinnyPartM".position.y < 16: #working
-		$"../ClippingMaskM/SpinnyPartTM".position.y = $"../ClippingMaskM/SpinnyPartM".position.y + 300
+	if SpinnyPartM > 16:
+		$"../ClippingMaskM/SpinnyPartTM".position.y = SpinnyPartM - 95
+	elif SpinnyPartM < 16: #working
+		$"../ClippingMaskM/SpinnyPartTM".position.y = SpinnyPartM + 300
 		
 	#Place the Other Half (Right)
-	if $"../ClippingMaskR/SpinnyPartR".position.y > 16:
-		$"../ClippingMaskR/SpinnyPartTR".position.y = $"../ClippingMaskR/SpinnyPartR".position.y - 95
-	elif $"../ClippingMaskR/SpinnyPartR".position.y < 16: #working
-		$"../ClippingMaskR/SpinnyPartTR".position.y = $"../ClippingMaskR/SpinnyPartR".position.y + 300
+	if SpinnyPartR > 16:
+		$"../ClippingMaskR/SpinnyPartTR".position.y = SpinnyPartR - 95
+	elif SpinnyPartR < 16: #working
+		$"../ClippingMaskR/SpinnyPartTR".position.y = SpinnyPartR + 300
 func _on_pressed() -> void:
 	_on_spin_button_pressed() # Replace with function body.
