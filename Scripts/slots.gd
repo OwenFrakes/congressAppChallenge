c4807e5122a183958b8720e9f5cbd2fdaab36783
JoshuaWorkman08 <joshua.workman@stu.daviess.kyschools.us>
2024-09-06 13:11:06 -0500

    Spinny Part Spins Correctly
    Both sprites move to their proper position per slot. Needs animation and needs gambling / score calculation now.

--- Scripts/slots.gd
+++ Scripts/slots.gd
@@ -7,10 +7,27 @@ func _ready() -> void:
 	
 func _on_spin_button_pressed() -> void:
 	print("button preseed")
-	$"../ClippingMaskL/SpinnyPartL".position.y = randi_range(173, 682)
-	$"../ClippingMaskM/SpinnyPartM".position.y = randi_range(173, 682)
-	$"../ClippingMaskR/SpinnyPartR".position.y = randi_range(173, 682)
-
-
+	#Randomize Base Positions
+	$"../ClippingMaskL/SpinnyPartL".position.y = randi_range(-173,189)
+	$"../ClippingMaskM/SpinnyPartM".position.y = randi_range(-173,189)
+	$"../ClippingMaskR/SpinnyPartR".position.y = randi_range(-173,189)
+	
+	#Place the Other Half (Left)
+	if $"../ClippingMaskL/SpinnyPartL".position.y > 16: 
+		$"../ClippingMaskL/SpinnyPartTL".position.y = $"../ClippingMaskL/SpinnyPartL".position.y - 95
+	elif $"../ClippingMaskL/SpinnyPartL".position.y < 16: #working
+		$"../ClippingMaskL/SpinnyPartTL".position.y = $"../ClippingMaskL/SpinnyPartL".position.y + 300
+		
+	#Place the Other Half (Middle)
+	if $"../ClippingMaskM/SpinnyPartM".position.y > 16:
+		$"../ClippingMaskM/SpinnyPartTM".position.y = $"../ClippingMaskM/SpinnyPartM".position.y - 95
+	elif $"../ClippingMaskM/SpinnyPartM".position.y < 16: #working
+		$"../ClippingMaskM/SpinnyPartTM".position.y = $"../ClippingMaskM/SpinnyPartM".position.y + 300
+		
+	#Place the Other Half (Right)
+	if $"../ClippingMaskR/SpinnyPartR".position.y > 16:
+		$"../ClippingMaskR/SpinnyPartTR".position.y = $"../ClippingMaskR/SpinnyPartR".position.y - 95
+	elif $"../ClippingMaskR/SpinnyPartR".position.y < 16: #working
+		$"../ClippingMaskR/SpinnyPartTR".position.y = $"../ClippingMaskR/SpinnyPartR".position.y + 300
 func _on_pressed() -> void:
 	_on_spin_button_pressed() # Replace with function body.
