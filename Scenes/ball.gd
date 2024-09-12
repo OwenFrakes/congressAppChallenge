74af1508df5ccb3827a5a4c9bdce1705db9e3b85
Jacob-Hagerman <jacob.hagerman@stu.daviess.kyschools.us>
2024-09-12 13:09:26 -0500

    Betting
    All outside bets are codded. I started coding all the individual numbers.

--- Scenes/ball.gd
+++ Scenes/ball.gd
@@ -7,8 +7,13 @@ var spin_velocity = 0
 var friction = 0.9
 #test varables for betting
 var bet_amount=0
-var bet="no"
+var bet_color="no"
 var money=10
+var bet_even_odd="no"
+var bet_1_to_18="no"
+var bet_12="no"
+var bet_2_to_1="no"
+var bet_number="no"
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	#make ball centered
@@ -48,124 +53,339 @@ func _process(delta):
 			#find the angle the ball stops at and prints the results
 			if new_angle%360>=5 && new_angle%360<=12||new_angle%360>=-354 && new_angle%360<=-345:
 				print("2, BLACK, EVEN, 1-18, 1st 12, 2nd 2 to 1")
-				bet="black"
+				bet_color="black"
+				bet_even_odd="even"
+				bet_1_to_18="1-18"
+				bet_12="1st"
+				bet_2_to_1="2nd"
+				bet_number="2"
 			elif new_angle%360>=13 && new_angle%360<=21||new_angle%360>=-344 && new_angle%360<=-334:
 				print("14, RED, EVEN, 1-18, 2nd 12, 2nd 2 to 1")
-				bet="red"
+				bet_color="red"
+				bet_even_odd="even"
+				bet_1_to_18="1-18"
+				bet_12="2nd"
+				bet_2_to_1="2nd"
+				bet_number="14"
 			elif new_angle%360>=22 && new_angle%360<=32||new_angle%360>=-333 && new_angle%360<=-325:
 				print("35, BLACK, ODD, 19-36, 3rd 12, 2nd 2 to 1")
-				bet="black"
+				bet_color="black"
+				bet_even_odd="odd"
+				bet_1_to_18="19-36"
+				bet_12="3rd"
+				bet_2_to_1="2nd"
+				bet_number="35"
 			elif new_angle%360>=33 && new_angle%360<=44||new_angle%360>=-324 && new_angle%360<=-316:
 				print("23, RED, ODD, 19-36, 2nd 12, 2nd 2 to 1")
-				bet="red"
+				bet_color="red"
+				bet_even_odd="odd"
+				bet_1_to_18="19-36"
+				bet_12="2nd"
+				bet_2_to_1="2nd"
+				bet_number="23"
 			elif new_angle%360>=45 && new_angle%360<=54||new_angle%360>=-315 && new_angle%360<=-304:
 				print("4, BLACK, EVEN, 1-18, 1st 12, 1st 2 to 1")
-				bet="black"
+				bet_color="black"
+				bet_even_odd="even"
+				bet_1_to_18="1-18"
+				bet_12="1st"
+				bet_2_to_1="1st"
+				bet_number="4"
 			elif new_angle%360>=55 && new_angle%360<=65||new_angle%360>=-303 && new_angle%360<=-295:
 				print("16, RED, EVEN, 1-18, 2nd 12, 1st 2 to 1")
-				bet="red"
+				bet_color="red"
+				bet_even_odd="even"
+				bet_1_to_18="1-18"
+				bet_12="2nd"
+				bet_2_to_1="1st"
+				bet_number="16"
 			elif new_angle%360>=66 && new_angle%360<=74||new_angle%360>=-294 && new_angle%360<=-285:
 				print("33, BLACK, ODD, 19-36, 3rd 12, 3rd 2 to 1")
-				bet="black"
+				bet_color="black"
+				bet_even_odd="odd"
+				bet_1_to_18="19-36"
+				bet_12="3rd"
+				bet_2_to_1="3rd"
+				bet_number="33"
 			elif new_angle%360>=75 && new_angle%360<=82||new_angle%360>=-284 && new_angle%360<=-278:
 				print("21, RED, ODD, 19-36, 2nd 12, 3rd 2 to 1")
-				bet="red"
+				bet_color="red"
+				bet_even_odd="odd"
+				bet_1_to_18="19-36"
+				bet_12="2nd"
+				bet_2_to_1="3rd"
+				bet_number="21"
 			elif new_angle%360>=83 && new_angle%360<=90||new_angle%360>=-277 && new_angle%360<=-269:
 				print("6, BLACK, EVEN, 1-18, 1st 12, 3rd 2 to 1")
-				bet="black"
+				bet_color="black"
+				bet_even_odd="even"
+				bet_1_to_18="1-18"
+				bet_12="1st"
+				bet_2_to_1="3rd"
+				bet_number="6"
 			elif new_angle%360>=91 && new_angle%360<=96||new_angle%360>=-268 && new_angle%360<=-260:
 				print("18, RED, EVEN, 1-18, 2nd 12, 3rd 2 to 1")
-				bet="red"
+				bet_color="red"
+				bet_even_odd="even"
+				bet_1_to_18="1-18"
+				bet_12="2nd"
+				bet_2_to_1="3rd"
+				bet_number="18"
 			elif new_angle%360>=97 && new_angle%360<=104||new_angle%360>=-259 && new_angle%360<=-252:
 				print("31, BLACK, ODD, 19-36, 3rd 12, 1st 2 to 1")
-				bet="black"
+				bet_color="black"
+				bet_even_odd="odd"
+				bet_1_to_18="19-36"
+				bet_12="3rd"
+				bet_2_to_1="1st"
+				bet_number="31"
 			elif new_angle%360>=105 && new_angle%360<=117||new_angle%360>=-251 && new_angle%360<=-243:
 				print("19, RED, ODD, 19-36, 2nd 12, 1st 2 to 1")
-				bet="red"
+				bet_color="red"
+				bet_even_odd="odd"
+				bet_1_to_18="19-36"
+				bet_12="2nd"
+				bet_2_to_1="1st"
+				bet_number="19"
 			elif new_angle%360>=118 && new_angle%360<=128||new_angle%360>=-242 && new_angle%360<=-232:
 				print("8, BLACK, EVEN, 1-18, 1st 12, 2nd 2 to 1")
-				bet="black"
+				bet_color="black"
+				bet_even_odd="even"
+				bet_1_to_18="1-18"
+				bet_12="1st"
+				bet_2_to_1="2nd"
+				bet_number="8"
 			elif new_angle%360>=129 && new_angle%360<=139||new_angle%360>=-231 && new_angle%360<=-221:
 				print("12, RED, EVEN, 1-18, 1st 12, 3rd 2 to 1")
-				bet="red"
+				bet_color="red"
+				bet_even_odd="even"
+				bet_1_to_18="1-18"
+				bet_12="1st"
+				bet_2_to_1="3rd"
+				bet_number="12"
 			elif new_angle%360>=140 && new_angle%360<=147||new_angle%360>=-220 && new_angle%360<=-212:
 				print("29, BLACK, ODD, 19-36, 3rd 12, 2nd 2 to 1")
-				bet="black"
+				bet_color="black"
+				bet_even_odd="odd"
+				bet_1_to_18="19-36"
+				bet_12="3rd"
+				bet_2_to_1="2nd"
+				bet_number="29"
 			elif new_angle%360>=148 && new_angle%360<=154||new_angle%360>=-211 && new_angle%360<=-202:
 				print("25, RED, ODD, 19-36, 3rd 12, 1st 2 to 1")
-				bet="red"
+				bet_color="red"
+				bet_even_odd="odd"
+				bet_1_to_18="19-36"
+				bet_12="3rd"
+				bet_2_to_1="1st"
+				bet_number="25"
 			elif new_angle%360>=155 && new_angle%360<=164||new_angle%360>=-201 && new_angle%360<=-190:
 				print("10, BLACK, EVEN, 1-18, 1st 12, 1st 2 to 1")
-				bet="black"
+				bet_color="black"
+				bet_even_odd="even"
+				bet_1_to_18="1-18"
+				bet_12="1st"
+				bet_2_to_1="1st"
+				bet_number="10"
 			elif new_angle%360>=165 && new_angle%360<=172||new_angle%360>=-189 && new_angle%360<=-183:
 				print("27, RED, ODD, 19-36, 3rd 12, 3rd 2 to 1")
-				bet="red"
+				bet_color="red"
+				bet_even_odd="odd"
+				bet_1_to_18="19-36"
+				bet_12="3rd"
+				bet_2_to_1="3rd"
+				bet_number="27"
 			elif new_angle%360>=-4 && new_angle%360<=4:
 				print("0, Green")
+				bet_number="0"
 			elif new_angle%360>=-175 && new_angle%360<=-167||new_angle%360>=184 && new_angle%360<=192:
 				print("1, RED, ODD, 1-18, 1st 12, 1st 2 to 1")
-				bet="red"
+				bet_color="red"
+				bet_even_odd="odd"
+				bet_1_to_18="1-18"
+				bet_12="1st"
+				bet_2_to_1="1st"
+				bet_number="1"
 			elif new_angle%360>=-166 && new_angle%360<=-158||new_angle%360>=193 && new_angle%360<=201:
 				print("13, BLACK, ODD, 1-18, 2nd 12, 1st 2 to 1")
-				bet="black"
+				bet_color="black"
+				bet_even_odd="odd"
+				bet_1_to_18="1-18"
+				bet_12="2nd"
+				bet_2_to_1="1st"
+				bet_number="13"
 			elif new_angle%360>=-157 && new_angle%360<=-147||new_angle%360>=202 && new_angle%360<=212:
 				print("36, RED, EVEN, 19-36, 3rd 12, 3rd 2 to 1")
-				bet="red"
+				bet_color="red"
+				bet_even_odd="even"
+				bet_1_to_18="19-36"
+				bet_12="3rd"
+				bet_2_to_1="3rd"
+				bet_number="36"
 			elif new_angle%360>=-146 && new_angle%360<=-138||new_angle%360>=213 && new_angle%360<=222:
 				print("24, BLACK, EVEN, 19-36, 2nd 12, 3rd 2 to 1")
-				bet="black"
+				bet_color="black"
+				bet_even_odd="even"
+				bet_1_to_18="19-36"
+				bet_12="2nd"
+				bet_2_to_1="3rd"
+				bet_number="24"
 			elif new_angle%360>=-137 && new_angle%360<=-126||new_angle%360>=223 && new_angle%360<=233:
 				print("3, RED, ODD, 1-18, 1st 12, 3rd 2 to 1")
-				bet="red"
+				bet_color="red"
+				bet_even_odd="odd"
+				bet_1_to_18="1-18"
+				bet_12="1st"
+				bet_2_to_1="3rd"
+				bet_number="3"
 			elif new_angle%360>=-125 && new_angle%360<=-115||new_angle%360>=234 && new_angle%360<=244:
 				print("15, BLACK, ODD, 1-18, 2nd 12, 3rd 2 to 1")
-				bet="black"
+				bet_color="black"
+				bet_even_odd="odd"
+				bet_1_to_18="1-18"
+				bet_12="2nd"
+				bet_2_to_1="3rd"
+				bet_number="15"
 			elif new_angle%360>=-114 && new_angle%360<=-106||new_angle%360>=245 && new_angle%360<=253:
 				print("34, RED, EVEN, 19-36, 3rd 12, 1st 2 to 1")
-				bet="red"
+				bet_color="red"
+				bet_even_odd="even"
+				bet_1_to_18="19-36"
+				bet_12="3rd"
+				bet_2_to_1="1st"
+				bet_number="34"
 			elif new_angle%360>=-105 && new_angle%360<=-98||new_angle%360>=254 && new_angle%360<=261:
 				print("22, BLACK, EVEN, 19-36, 2nd 12, 1st 2 to 1")
-				bet="black"
+				bet_color="black"
+				bet_even_odd="even"
+				bet_1_to_18="19-36"
+				bet_12="2nd"
+				bet_2_to_1="1st"
+				bet_number="22"
 			elif new_angle%360>=-97 && new_angle%360<=-89||new_angle%360>=262 && new_angle%360<=269:
 				print("5, RED, ODD, 1-18, 1st 12, 2nd 2 to 1")
-				bet="red"
+				bet_color="red"
+				bet_even_odd="odd"
+				bet_1_to_18="1-18"
+				bet_12="1st"
+				bet_2_to_1="2nd"
+				bet_number="5"
 			elif new_angle%360>=-88 && new_angle%360<=-81||new_angle%360>=270 && new_angle%360<=278:
 				print("17, BLACK, ODD, 1-18, 2nd 12, 2nd 2 to 1")
-				bet="black"
+				bet_color="black"
+				bet_even_odd="odd"
+				bet_1_to_18="1-18"
+				bet_12="2nd"
+				bet_2_to_1="2nd"
+				bet_number="17"
 			elif new_angle%360>=-80 && new_angle%360<=-73||new_angle%360>=279 && new_angle%360<=285:
 				print("32, RED, EVEN, 19-36, 3rd 12, 2nd 2 to 1")
-				bet="red"
+				bet_color="red"
+				bet_even_odd="even"
+				bet_1_to_18="19-36"
+				bet_12="3rd"
+				bet_2_to_1="2nd"
+				bet_number="32"
 			elif new_angle%360>=-72 && new_angle%360<=-65||new_angle%360>=286 && new_angle%360<=295:
 				print("20, BLACK, EVEN, 19-36, 2nd 12, 2nd 2 to 1")
-				bet="black"
+				bet_color="black"
+				bet_even_odd="even"
+				bet_1_to_18="19-36"
+				bet_12="2nd"
+				bet_2_to_1="2nd"
+				bet_number="20"
 			elif new_angle%360>=-64 && new_angle%360<=-54||new_angle%360>=296 && new_angle%360<=305:
 				print("7, RED, ODD, 1-18, 1st 12, 1st 2 to 1")
-				bet="red"
+				bet_color="red"
+				bet_even_odd="odd"
+				bet_1_to_18="1-18"
+				bet_12="1st"
+				bet_2_to_1="1st"
+				bet_number="7"
 			elif new_angle%360>=-53 && new_angle%360<=-43||new_angle%360>=306 && new_angle%360<=316:
 				print("11, BLACK, ODD, 1-18, 1st 12, 2nd 2 to 1")
-				bet="black"
+				bet_color="black"
+				bet_even_odd="odd"
+				bet_1_to_18="1-18"
+				bet_12="1st"
+				bet_2_to_1="2nd"
+				bet_number="11"
 			elif new_angle%360>=-42 && new_angle%360<=-33||new_angle%360>=317 && new_angle%360<=325:
 				print("30, RED, EVEN, 19-36, 3rd 12, 3rd 2 to 1")
-				bet="red"
+				bet_color="red"
+				bet_even_odd="even"
+				bet_1_to_18="19-36"
+				bet_12="3rd"
+				bet_2_to_1="3rd"
+				bet_number="30"
 			elif new_angle%360>=-32 && new_angle%360<=-26||new_angle%360>=326 && new_angle%360<=334:
 				print("26, BLACK, EVEN, 19-36, 3rd 12, 2nd 2 to 1")
-				bet="black"
+				bet_color="black"
+				bet_even_odd="even"
+				bet_1_to_18="19-36"
+				bet_12="3rd"
+				bet_2_to_1="2nd"
+				bet_number="26"
 			elif new_angle%360>=-25 && new_angle%360<=-15||new_angle%360>=335 && new_angle%360<=345:
 				print("9, RED, ODD, 1-18, 1st 12, 3rd 2 to 1")
-				bet="red"
+				bet_color="red"
+				bet_even_odd="odd"
+				bet_1_to_18="1-18"
+				bet_12="1st"
+				bet_2_to_1="3rd"
+				bet_number="9"
 			elif new_angle%360>=-14 && new_angle%360<=-5||new_angle%360>=346 && new_angle%360<=354:
 				print("28, BLACK, EVEN, 19-36, 3rd 12, 1st 2 to 1")
+				bet_color="black"
+				bet_even_odd="even"
+				bet_1_to_18="19-36"
+				bet_12="3rd"
+				bet_2_to_1="1st"
+				bet_number="28"
 			else: 
 				print("00, Green")
+				bet_number="00"
 			var table=$"../Table"
 			bet_amount=table.bet_amount
-			if bet==table.bet:
+			if bet_color==table.bet_color:
+				#pays 1-1
 				money+=bet_amount
-				bet="no"
+				bet_color="no"
+				table.bet_amount=0
+				print(money)
+			elif bet_even_odd==table.bet_even_odd:
+				#pays 1-1
+				money+=bet_amount
+				bet_even_odd="no"
+				table.bet_amount=0
+				print(money)
+			elif bet_1_to_18==table.bet_1_to_18:
+				#pays 1-1
+				money+=bet_amount
+				bet_1_to_18="no"
+				table.bet_amount=0
+				print(money)
+			elif bet_12==table.bet_12:
+				#pays 2-1
+				money+=bet_amount*2
+				bet_12="no"
+				table.bet_amount=0
+				print(money)
+			elif bet_2_to_1==table.bet_2_to_1:
+				#pays 2-1
+				money+=bet_amount*2
+				bet_2_to_1="no"
+				table.bet_amount=0
+				print(money)
+			elif bet_number==table.bet_number:
+				#pays 35-1
+				money+=bet_amount*35
+				bet_number="no"
 				table.bet_amount=0
 				print(money)
 			else:
 				money-=bet_amount
+				table.bet_amount=0
 				print(money)
 			
 func on_button_pressed():
--- Scenes/table.gd
+++ Scenes/table.gd
@@ -1,43 +1,258 @@
 extends Sprite2D
-#create varables for all buttons
+#create varables for all bets
 var bet_placed=false
 var bet_amount=0
-var bet=""
+var bet_color=""
+var bet_even_odd=""
+var bet_1_to_18=""
+var bet_12=""
+var bet_2_to_1=""
+var bet_number=""
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var one_node = $"1"
-	one_node.connect("pressed", Callable(self, "one_button_pressed"))
-	var red_node = $RED
-	red_node.connect("pressed", Callable(self, "red_button_pressed"))
+	#black
 	var black_node = $Black
 	black_node.connect("pressed", Callable(self, "black_button_pressed"))
+	#red
+	var red_node = $RED
+	red_node.connect("pressed", Callable(self, "red_button_pressed"))
+	#even
+	var even_node = $EVEN
+	even_node.connect("pressed", Callable(self, "even_button_pressed"))
+	#odd
+	var odd_node = $ODD
+	odd_node.connect("pressed", Callable(self, "odd_button_pressed"))
+	#1-18
+	var to_18_node = $"1 to 18"
+	to_18_node.connect("pressed", Callable(self, "to_18_button_pressed"))
+	#19-36
+	var to_36_node = $"19 to 36"
+	to_36_node.connect("pressed", Callable(self, "to_36_button_pressed"))
+	#1st 12
+	var b1_12_node = $"1st 12"
+	b1_12_node.connect("pressed", Callable(self, "b1_12_button_pressed"))
+	#2nd 12
+	var b2_12_node = $"2nd 12"
+	b2_12_node.connect("pressed", Callable(self, "b2_12_button_pressed"))
+	#3rd 12
+	var b3_12_node = $"3rd 12"
+	b3_12_node.connect("pressed", Callable(self, "b3_12_button_pressed"))
+	#1st 2-1
+	var b1_2_1_node = $"1st 2 to 1"
+	b1_2_1_node.connect("pressed", Callable(self, "b1_2_1_button_pressed"))
+	#2nd 2-1
+	var b2_2_1_node = $"2nd 2 to 1"
+	b2_2_1_node.connect("pressed", Callable(self, "b2_2_1_button_pressed"))
+	#3rd 2-1
+	var b3_2_1_node = $"3rd 2 to 1"
+	b3_2_1_node.connect("pressed", Callable(self, "b3_2_1_button_pressed"))
+	#spin
 	var button_node = $"../Button"
 	button_node.connect("pressed", Callable(self, "on_button_pressed"))
+	#reset
 	var reset_node = $"../Reset"
 	reset_node.connect("pressed", Callable(self, "on_reset_pressed"))
-	
+	#1
+	var one_node = $"1"
+	one_node.connect("pressed", Callable(self, "one_button_pressed"))
+	#2
+	var b2_node = $"2"
+	b2_node.connect("pressed", Callable(self, "b2_button_pressed"))
+	#3
+	var b3_node = $"3"
+	b3_node.connect("pressed", Callable(self, "b3_button_pressed"))
+	#4
+	var b4_node = $"4"
+	b4_node.connect("pressed", Callable(self, "b4_button_pressed"))
+	#5
+	var b5_node = $"5"
+	b5_node.connect("pressed", Callable(self, "b5_button_pressed"))
+	#6
+	var b6_node = $"6"
+	b6_node.connect("pressed", Callable(self, "b6_button_pressed"))
+	#7
+	var b7_node = $"7"
+	b7_node.connect("pressed", Callable(self, "b7_button_pressed"))
+	#8
+	var b8_node =$"8"
+	b8_node.connect("pressed", Callable(self, "b8_button_pressed"))
+	#9
+	var b9_node =$"9"
+	b9_node.connect("pressed", Callable(self, "b9_button_pressed"))
+	#10
+	var b10_node = $"10"
+	b10_node.connect("pressed", Callable(self, "b10_button_pressed"))
+	#11
+	var b11_node = $"11"
+	b11_node.connect("pressed", Callable(self, "b11_button_pressed"))
+	#12
+	var b12_node =$"12"
+	b12_node.connect("pressed", Callable(self, "b12_button_pressed"))
+	#13
+	var b13_node =$"13"
+	b13_node.connect("pressed", Callable(self, "b13_button_pressed"))
+	#14
+	var b14_node = $"14"
+	b14_node.connect("pressed", Callable(self, "b14_button_pressed"))
+	#15
+	var b15_node = $"15"
+	b15_node.connect("pressed", Callable(self, "b15_button_pressed"))
+	#16
+	var b16_node =$"16"
+	b16_node.connect("pressed", Callable(self, "b16_button_pressed"))
+	#17
+	var b17_node = $"17"
+	b17_node.connect("pressed", Callable(self, "b17_button_pressed"))
+	#18
+	var b18_node = $"18"
+	b18_node.connect("pressed", Callable(self, "b18_button_pressed"))
+	#19
+	var b19_node = $"19"
+	b19_node.connect("pressed", Callable(self, "b19_button_pressed"))
+	#20
+	var b20_node = $"20"
+	b20_node.connect("pressed", Callable(self, "b20_button_pressed"))
+	#21
+	var b21_node = $"21"
+	b21_node.connect("pressed", Callable(self, "b21_button_pressed"))
+	#22
+	var b22_node = $"22"
+	b22_node.connect("pressed", Callable(self, "b22_button_pressed"))
+	#23
+	var b23_node = $"23"
+	b23_node.connect("pressed", Callable(self, "b23_button_pressed"))
+	#24
+	var b24_node = $"24"
+	b24_node.connect("pressed", Callable(self, "b24_button_pressed"))
+	#25
+	var b25_node = $"25"
+	b25_node.connect("pressed", Callable(self, "b25_button_pressed"))
+	#26
+	var b26_node = $"26"
+	b26_node.connect("pressed", Callable(self, "b26_button_pressed"))
+	#27
+	var b27_node = $"27"
+	b27_node.connect("pressed", Callable(self, "b27_button_pressed"))
+	##28
+	#var one_node = $"1"
+	#one_node.connect("pressed", Callable(self, "one_button_pressed"))
+	##29
+	#var one_node = $"1"
+	#one_node.connect("pressed", Callable(self, "one_button_pressed"))
+	##30
+	#var one_node = $"1"
+	#one_node.connect("pressed", Callable(self, "one_button_pressed"))
+	##31
+	#var one_node = $"1"
+	#one_node.connect("pressed", Callable(self, "one_button_pressed"))
+	##32
+	#var one_node = $"1"
+	#one_node.connect("pressed", Callable(self, "one_button_pressed"))
+	##33
+	#var one_node = $"1"
+	#one_node.connect("pressed", Callable(self, "one_button_pressed"))
+	##34
+	#var one_node = $"1"
+	#one_node.connect("pressed", Callable(self, "one_button_pressed"))
+	##35
+	#var one_node = $"1"
+	#one_node.connect("pressed", Callable(self, "one_button_pressed"))
+	##36
+	#var one_node = $"1"
+	#one_node.connect("pressed", Callable(self, "one_button_pressed"))
+	##0
+	#var one_node = $"1"
+	#one_node.connect("pressed", Callable(self, "one_button_pressed"))
+	##00
+	#var one_node = $"1"
+	#one_node.connect("pressed", Callable(self, "one_button_pressed"))
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta) -> void:
 	pass
-	
+#code for black 
 func black_button_pressed():
 	$Chip.show()
 	bet_placed=true
 	bet_amount+=1
-	bet="black"
-
-func on_button_pressed():
-	self.hide()
-	$Chip.hide()
-func one_button_pressed():
-	$Chip.show()
-	bet_placed=true
-func on_reset_pressed():
-	self.show()
-	bet_placed=false
-	bet_amount=0
+	bet_color="black"
+#code for red
 func red_button_pressed():
 	$Chip.show()
 	bet_placed=true
 	bet_amount+=1
-	bet="red"
+	bet_color="red"
+#code for even
+func even_button_pressed():
+	$Chip.show()
+	bet_placed=true
+	bet_amount+=1
+	bet_even_odd="even"
+#code for odd
+func odd_button_pressed():
+	$Chip.show()
+	bet_placed=true
+	bet_amount+=1
+	bet_even_odd="odd"
+#code for 1 to 18
+func to_18_button_pressed():
+	$Chip.show()
+	bet_placed=true
+	bet_amount+=1
+	bet_1_to_18="1-18"
+#code for 19 to 36
+func to_36_button_pressed():
+	$Chip.show()
+	bet_placed=true
+	bet_amount+=1
+	bet_1_to_18="19-36"
+#1st 12
+func b1_12_button_pressed():
+	$Chip.show()
+	bet_placed=true
+	bet_amount+=1
+	bet_12="1st"
+#2nd 12
+func b2_12_button_pressed():
+	$Chip.show()
+	bet_placed=true
+	bet_amount+=1
+	bet_12="2nd"
+#3rd 12
+func b3_12_button_pressed():
+	$Chip.show()
+	bet_placed=true
+	bet_amount+=1
+	bet_12="3rd"
+#1st 2-1
+func b1_2_1_button_pressed():
+	$Chip.show()
+	bet_placed=true
+	bet_amount+=1
+	bet_2_to_1="1st"
+#2nd 2-1
+func b2_2_1_button_pressed():
+	$Chip.show()
+	bet_placed=true
+	bet_amount+=1
+	bet_2_to_1="2nd"
+#3rd 2-1
+func b3_2_1_button_pressed():
+	$Chip.show()
+	bet_placed=true
+	bet_amount+=1
+	bet_2_to_1="3rd"
+#code to go to the wheel
+func on_button_pressed():
+	self.hide()
+	$Chip.hide()
+#code to reset the wheel
+func on_reset_pressed():
+	self.show()
+	bet_placed=false
+	bet_amount=0
+#code for 1
+func one_button_pressed():
+	$Chip.show()
+	bet_placed=true
+	bet_amount+=1
+	bet_number="1"
