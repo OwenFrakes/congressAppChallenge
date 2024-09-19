86acb3f50b7f064f657acf5823aea26af0754c41
Owen Frakes <Owen.Frakes@stu.daviess.kyschools.us>
2024-09-19 14:49:51 -0500

    Roulette Graphics + Labels

--- Scripts/easyMoneyLabel.gd
+++ Scripts/easyMoneyLabel.gd
@@ -1,6 +1,8 @@
 extends Label
 
+func _ready() -> void:
+	theme = load("res://Resources/Themes/LabelThemes/LabelGeneralTheme.tres")
+
 func _process(_delta: float) -> void:
 	await get_tree().create_timer(0.1).timeout
 	text = "Player Money: " + str(StaticPlayerVariables.globalPlayerMoney)
-	print("ran")
