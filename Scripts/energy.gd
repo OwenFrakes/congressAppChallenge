af63585d297bde41f7444a0a3af04239154b3509
Owen Frakes <Owen.Frakes@stu.daviess.kyschools.us>
2024-09-30 14:38:40 -0500

    Energy Fixed, again.

--- Scripts/energy.gd
+++ Scripts/energy.gd
@@ -8,12 +8,12 @@ func _ready():
 	scale = Vector2(4.942,3.711)
 	z_index = 100
 	
-	while(true):
+	while(StaticPlayerVariables.playerEnergy > 0):
 		#Calculate Alpha
 		var alpha = -(StaticPlayerVariables.playerEnergy/6.0) + 1
 		
 		self_modulate = Color(1,1,1,alpha)
 		
-		if(typeof(get_tree) != null):
-			await get_tree().create_timer(0.1).timeout
+		await get_tree().create_timer(0.1).timeout
 	
+	visible = false
