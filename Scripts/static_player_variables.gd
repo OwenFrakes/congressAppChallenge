a25cb4136a494881c1e5456080c16ca8807fb12c
Owen Frakes <Owen.Frakes@stu.daviess.kyschools.us>
2024-10-04 13:06:39 -0500

    Update static_player_variables.gd

--- Scripts/static_player_variables.gd
+++ Scripts/static_player_variables.gd
@@ -3,12 +3,12 @@ extends Node
 var playerEnergy = 6
 var globalPlayerMoney = 100 # The amount of money the player has.
 var dayCount = 1 # The day, 1 is monday.
-var spouseState = "Healty"
+var spouseState = "Healthy"
 
 #Children States
-var child1State = "Healty"
-var child2State = "Healty"
-var child3State = "Healty"
+var child1State = "Healthy"
+var child2State = "Healthy"
+var child3State = "Healthy"
 
 #Children Health
 var child1Health = 4
